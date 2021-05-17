2460580832fddf32a8dc8d00a439c045907aaf1f
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-05-17 15:59:03 -0300

    Add files via upload

---
+++ WaterTV.py
@@ -0,0 +1,19 @@
+bicycles = ['trek', 'cannondale', "redline", 'specialized']
+print(bicycles[1])
+print(bicycles[0].title())
+print(bicycles[0].upper())
+print(bicycles[0].lower())
+print(bicycles[-3])
+print("my bike with a"+" "+bicycles[2]+" "+"are very cool")
+dogs=["amora","cacau"]
+print(dogs[0]+" "+dogs[1])
+frase=["my dogs its","and them are very cute"]
+message=frase[0]+" "+dogs[0]+" "+dogs[1]+" "+frase[1]
+print(message)
+#linha
+dogs[0]="cacau"
+dogs[1]="amora"
+print(dogs)
+dogs.append("piasta")
+print(dogs)
+#cacau amora piasta
